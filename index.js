561a9cf8ba6f9787f252fe2f7f35c08d95c142fb
kadas36 <oduorthomas14@gmail.com>
2020-09-17 02:33:46 +0300

    Modal created

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-document.getElementById('button').addEventListener('click',function(){
-    document.querySelector('bg-modal').getElementsByClassName.display = 'flex'
-});
-
-document.querySelector('.close').addEventListener('click',function(){
-    document.querySelector('bg-modal').getElementsByClassName.display = 'none'
-});
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myButton");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+  };
+span.onclick = function() {
+  modal.style.display = "none";
+};
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+      
