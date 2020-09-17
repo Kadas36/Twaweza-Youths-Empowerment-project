3d2e812916703d15a94ac452480a235518d5f808
kadas36 <oduorthomas14@gmail.com>
2020-09-17 11:45:29 +0300

    Updated footer

--- index.js
+++ index.js
@@ -4,10 +4,4 @@ document.getElementById("myButton").onclick = function() {
 
 document.getElementsByClassName("close")[0].onclick = function() {
 document.getElementById("myModal").style.display = "none";
-};
-
-// window.onclick = function(event) {
-//   if (event.target == document.getElementById("myModal")) {
-//     document.getElementById("myModal").style.display = "none";
-//   }
-// }      
+}   
